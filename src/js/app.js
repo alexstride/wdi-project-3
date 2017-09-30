4a91fc51a06af2e8a406b4caea1de8465c811845
Mardzy <mardlingp@gmail.com>
2017-09-30 15:00:58 +0100

    create navbar prototype

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 // app.js is the main JS file which you should define your Angular modu
 
 angular
-  .module('tandem', ['ui.router', 'ngResource', 'satellizer', 'ngMessages']);
+  .module('tandem', ['ui.router', 'ngResource', 'satellizer', 'ngMessages', 'ui.bootstrap', 'ngAnimate']);
