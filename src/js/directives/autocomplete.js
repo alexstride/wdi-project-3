3492876de87393506df0ce684a5fd6f088e1d736
CaptainBrad <bradleylebetkin@gmail.com>
2017-10-01 17:14:51 +0100

    index stuff

--- src/js/directives/autocomplete.js
+++ src/js/directives/autocomplete.js
@@ -7,18 +7,19 @@ angular
 function autocomplete(){
   return{
     restrict: 'A',
-    location: '='
-  },
+    scope: {
+      location: '='
+    },
+    require: 'ngModel',
+    link(scope,element, attrs, ngModel){
+      const input = document.querySelector('#place');
+      const autocomplete = new google.maps.places.Autocomplete(input, { types: ['establishment'] });
 
+      autocomplete.addListener('place_changed', () => {
+        scope.location = autocomplete.getPlace().geometry.toJSON();
+        ngModel.$setViewValue(element.val());
+      });
+    }
 
-
-
-  document.addEventListener('DOMContentLoaded', () => {
-    const input = document.querySelector('#place');
-    const autocomplete = new google.maps.places.Autocomplete(input, { types: ['establishment'] });
-
-    autocomplete.addListener('place_changed', () => {
-      console.log(autocomplete.getPlace());
-    });
-  });
+  };
 }
--- src/js/directives/googleMap.js
+++ src/js/directives/googleMap.js
@@ -1,3 +1,5 @@
+
+
 angular
   .module('tandem')
   .directive('googleMap', googleMap);
@@ -18,9 +20,20 @@ function googleMap($window) {
         center: {lat: 0, lng: 0}
       });
 
+
+      // CaptainBrad - trying to get marker into my locations
+      // need to do the global google thingy
+      
+      // const latLng = { lat: location.lat, lng: location.lng};
+      // const marker = new google.maps.Marker({
+      //   position: latLng,
+      //   map: map
+      // });
+
       scope.$watch('center', () => {
         if(!scope.center) return false;
         map.setCenter({lat: scope.center.lat, lng: scope.center.lng});
+        // marker.setPosition(scope.center); // marker stuff
       });
     }
   };
