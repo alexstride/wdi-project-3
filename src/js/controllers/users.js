ae29b87cef82610aa26e45e556394e1e263a7908
Mardzy <mardlingp@gmail.com>
2017-10-01 17:22:56 +0100

    add

--- src/js/controllers/users.js
+++ src/js/controllers/users.js
@@ -2,13 +2,34 @@ angular
   .module('tandem')
   .controller('UsersShowCtrl', UsersShowCtrl);
 
-UsersShowCtrl.$inject = [ 'User', '$stateParams' ];
-function UsersShowCtrl(User, $stateParams) {
+UsersShowCtrl.$inject = [ 'User', '$state' ];
+function UsersShowCtrl(User, $state) {
   const vm = this;
 
+  vm.user = User.get($state.params);
+
+  function usersDelete() {
+    vm.user
+      .$remove()
+      .then(() => $state.go('home'));
+  }
+
+  vm.delete = usersDelete;
+
+}
+
+
+UsersEditCtrl.$inject = ['User', '$stateParams', '$state'];
+function UsersEditCtrl(User, $stateParams, $state) {
+  const vm = this;
 
   vm.user = User.get($stateParams);
 
-  console.log('Single user: ', vm.user);
+  function usersUpdate() {
+    vm.user
+      .$update()
+      .then(() => $state.go('usersShow', $stateParams));
+  }
 
+  vm.update = usersUpdate;
 }
