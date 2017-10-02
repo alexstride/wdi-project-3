5d18823edfa39bb39d8218182520de7ea1b8f924
alexstride <stridealexander@gmail.com>
2017-10-02 12:10:29 +0100

    new directive created to hold map and information

---
+++ src/js/directives/editableRouteMap.js
@@ -0,0 +1,39 @@
+
+
+angular
+  .module('tandem')
+  .directive('editableRouteMap', editableRouteMap);
+
+editableRouteMap.inject = ['$window'];
+function editableRouteMap($window) {
+  return {
+    restrict: 'E',
+    replace: true,
+    template: '<div class="editable-map"><div class="map-holder">GOOGLE MAP GOES HERE</div><div class="map-aside"></div></div>',
+    scope: {
+      mapVar: '=',
+      rideInfo: '='
+    },
+    link($scope, element) {
+      console.log(element[0].querySelector('.map-holder'));
+      const mapElement = element[0].querySelector('.map-holder');
+      console.log($scope);
+      $scope.mapVar = new $window.google.maps.Map(mapElement, {
+        zoom: 14,
+        center: {lat: 0, lng: 0}
+      });
+
+      $scope.$watch('rideInfo', () => {
+        console.log('running');
+        console.log('value of rideInfo: ', $scope.rideInfo);
+        if(!$scope.rideInfo) return false;
+        $scope.mapVar.setCenter({lat: $scope.rideInfo.startPoint.lat, lng: $scope.rideInfo.startPoint.lng});
+        const marker = new $window.google.maps.Marker({
+          map: $scope.mapVar
+        });
+        marker.setPosition({lat: $scope.rideInfo.startPoint.lat, lng: $scope.rideInfo.startPoint.lng});
+      });
+
+    }
+  };
+}
