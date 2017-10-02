30a468578042e8e896e91d14b3314bc356660053
CaptainBrad <bradleylebetkin@gmail.com>
2017-10-02 15:33:19 +0100

    poops on map

--- src/js/directives/googleMapIndex.js
+++ src/js/directives/googleMapIndex.js
@@ -1,43 +1,74 @@
-// /* global google */
+/* global google */
 
-// angular
-//   .module('tandem')
-//   .directive('googleMapIndex', googleMapIndex);
-//
-// googleMapIndex.inject = ['$window'];
-// function googleMapIndex($window) {
-//   return {
-//     restrict: 'E',
-//     replace: true,
-//     template: '<div class="google-map">GOOGLE MAP GOES HERE</div>',
-//     scope: {
-//       mapVar: '=',
-//       rideInfo: '='
-//     },
-//     link($scope, element) {
-//       console.log($scope);
-//       $scope.mapVar = new $window.google.maps.Map(element[0], {
-//         zoom: 14,
-//         center: {lat: 51.51724, lng: -0.09728}
-//       });
-//
-//       $scope.$watch('rideInfo', () => {
-//         console.log('running');
-//         console.log('value of rideInfo: ', $scope.rideInfo);
-//         if(!$scope.rideInfo) return false;
-//         $scope.mapVar.setCenter({lat: $scope.rideInfo.startPoint.lat, lng: $scope.rideInfo.startPoint.lng});
-//         const marker = new $window.google.maps.Marker({
-//           map: $scope.mapVar,
-//           animation: google.maps.Animation.BOUNCE,
-//     icon: {
-//       url: 'https://cdn.shopify.com/s/files/1/1061/1924/files/Poop_Emoji.png?9898922749706957214',
-//       scaledSize: new google.maps.Size(10,10)
-//     }
-//
-//         });
-//         marker.setPosition({lat: $scope.rideInfo.startPoint.lat, lng: $scope.rideInfo.startPoint.lng});
-//       });
-//
-//     }
-//   };
-// }
+angular
+  .module('tandem')
+  .directive('googleMapIndex', googleMapIndex);
+
+googleMapIndex.inject = ['$window'];
+function googleMapIndex($window) {
+  return {
+    restrict: 'E',
+    replace: true,
+    template: '<div class="google-map">GOOGLE MAP GOES HERE</div>',
+    scope: {
+      mapVar: '=',
+      rideArray: '='
+    },
+    link($scope, element) {
+      console.log($scope);
+      $scope.mapVar = new $window.google.maps.Map(element[0], {
+        zoom: 14,
+        center: {lat: 51.51724, lng: -0.09728}
+      });
+
+      $scope.$watch('rideArray', () => {
+        if(!$scope.rideArray) return false;
+        $scope.mapVar.setCenter({ lat: 51.51724, lng: -0.09728});
+
+        console.log('ride array from directive', $scope.rideArray);
+        $scope.rideArray.forEach(ride => {
+          console.log('INSIDE LOOP!');
+          const marker = new $window.google.maps.Marker({
+            map: $scope.mapVar,
+            animation: google.maps.Animation.BOUNCE,
+            icon: {
+              url: 'https://cdn.shopify.com/s/files/1/1061/1924/files/Poop_Emoji.png?9898922749706957214',
+              scaledSize: new google.maps.Size(30,30)
+            }
+          // const marker = new $window.google.maps.Marker({
+          //   map: $scope.mapVar,
+          //   animation: google.maps.Animation.BOUNCE,
+          //   icon: {
+          //     url: 'https://cdn.shopify.com/s/files/1/1061/1924/files/Poop_Emoji.png?9898922749706957214',
+          //     scaledSize: new google.maps.Size(30,30)
+          //   }
+
+          });
+
+          const markerTwo = new $window.google.maps.Marker({
+            map: $scope.mapVar,
+            animation: google.maps.Animation.BOUNCE,
+            icon: {
+              url: 'https://cdn.shopify.com/s/files/1/1061/1924/files/Poop_Emoji.png?9898922749706957214',
+              scaledSize: new google.maps.Size(30,30)
+            }
+          // const marker = new $window.google.maps.Marker({
+          //   map: $scope.mapVar,
+          //   animation: google.maps.Animation.BOUNCE,
+          //   icon: {
+          //     url: 'https://cdn.shopify.com/s/files/1/1061/1924/files/Poop_Emoji.png?9898922749706957214',
+          //     scaledSize: new google.maps.Size(30,30)
+          //   }
+
+          });
+
+
+          marker.setPosition({ lat: ride.endPoint.lat, lng: ride.endPoint.lng });
+          markerTwo.setPosition({ lat: ride.startPoint.lat, lng: ride.startPoint.lng });
+        });
+
+      }, true);
+
+    }
+  };
+}
