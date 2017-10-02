b2121bbfa46562ce2a733cedd6940aea840a1a23
CaptainBrad <bradleylebetkin@gmail.com>
2017-10-02 17:16:46 +0100

    lines on a map

--- src/js/directives/googleMapIndex.js
+++ src/js/directives/googleMapIndex.js
@@ -11,62 +11,75 @@ function googleMapIndex($window) {
     replace: true,
     template: '<div class="google-map">GOOGLE MAP GOES HERE</div>',
     scope: {
-      mapVar: '=',
       rideArray: '='
     },
     link($scope, element) {
-      console.log($scope);
-      $scope.mapVar = new $window.google.maps.Map(element[0], {
+      const numColors = 40;
+      let colorArray = [];
+      function randomColor() {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+      }
+      function generateRandomColors(num) {
+        const arr = [];
+        for (let i = 0; i < num; i++) {
+          arr.push(randomColor());
+        }
+        return arr;
+      }
+      colorArray = generateRandomColors(numColors);
+      const map = new $window.google.maps.Map(element[0], {
         zoom: 14,
         center: {lat: 51.51724, lng: -0.09728}
       });
 
+      const directionsService = new google.maps.DirectionsService();
+
       $scope.$watch('rideArray', () => {
         if(!$scope.rideArray) return false;
-        $scope.mapVar.setCenter({ lat: 51.51724, lng: -0.09728});
+        map.setCenter({ lat: 51.51724, lng: -0.09728});
 
         console.log('ride array from directive', $scope.rideArray);
-        $scope.rideArray.forEach(ride => {
-          console.log('INSIDE LOOP!');
-          const marker = new $window.google.maps.Marker({
-            map: $scope.mapVar,
-            animation: google.maps.Animation.BOUNCE,
+        $scope.rideArray.forEach((ride, i) => {
+
+          const start = new $window.google.maps.Marker({
+            map: map,
+            animation: google.maps.Animation.DROP,
             icon: {
               url: 'https://cdn.shopify.com/s/files/1/1061/1924/files/Poop_Emoji.png?9898922749706957214',
               scaledSize: new google.maps.Size(30,30)
-            }
-          // const marker = new $window.google.maps.Marker({
-          //   map: $scope.mapVar,
-          //   animation: google.maps.Animation.BOUNCE,
-          //   icon: {
-          //     url: 'https://cdn.shopify.com/s/files/1/1061/1924/files/Poop_Emoji.png?9898922749706957214',
-          //     scaledSize: new google.maps.Size(30,30)
-          //   }
-
+            },
+            position: { lat: ride.startPoint.lat, lng: ride.startPoint.lng }
           });
 
-          const markerTwo = new $window.google.maps.Marker({
-            map: $scope.mapVar,
-            animation: google.maps.Animation.BOUNCE,
+          const end = new $window.google.maps.Marker({
+            map: map,
+            animation: google.maps.Animation.DROP,
             icon: {
-              url: 'https://cdn.shopify.com/s/files/1/1061/1924/files/Poop_Emoji.png?9898922749706957214',
-              scaledSize: new google.maps.Size(30,30)
-            }
-          // const marker = new $window.google.maps.Marker({
-          //   map: $scope.mapVar,
-          //   animation: google.maps.Animation.BOUNCE,
-          //   icon: {
-          //     url: 'https://cdn.shopify.com/s/files/1/1061/1924/files/Poop_Emoji.png?9898922749706957214',
-          //     scaledSize: new google.maps.Size(30,30)
-          //   }
-
+              url: '../images/bike-icon.png',
+              scaledSize: new google.maps.Size(50,50)
+            },
+            position: { lat: ride.endPoint.lat, lng: ride.endPoint.lng }
           });
 
+          const directionsDisplay = new google.maps.DirectionsRenderer({
+            suppressBicyclingLayer: true,
+            suppressMarkers: true,
+            polylineOptions: {
+              strokeColor: colorArray[i],
+              strokeOpacity: 1.0
+            }
+          });
+          directionsDisplay.setMap(map);
 
-          marker.setPosition({ lat: ride.endPoint.lat, lng: ride.endPoint.lng });
-          markerTwo.setPosition({ lat: ride.startPoint.lat, lng: ride.startPoint.lng });
+          directionsService.route({
+            origin: start.getPosition(),
+            destination: end.getPosition(),
+            travelMode: 'BICYCLING'
+          }, response => directionsDisplay.setDirections(response));
         });
-
       }, true);
 
     }
