b061c516ea63aa2e7ef3f6a033a3addceedbe8a9
alexstride <stridealexander@gmail.com>
2017-10-04 11:51:58 +0100

    ngModel fixed within the coordinate array

--- src/js/directives/autocomplete.js
+++ src/js/directives/autocomplete.js
@@ -9,6 +9,10 @@ function autocomplete(){
     restrict: 'A',
     require: 'ngModel',
     replace: false,
+    scope: {
+      lat: '=',
+      lng: '='
+    },
     link($scope, element, attrs, model){
 
       const autocomplete = new google.maps.places.Autocomplete(element[0]);
@@ -17,6 +21,8 @@ function autocomplete(){
         const place = autocomplete.getPlace();
         console.log('Changing location');
         const latLng = place.geometry.location.toJSON();
+        $scope.lat = latLng.lat;
+        $scope.lng = latLng.lng;
         // update the value of ng-model on the element to be the latLng
         model.$setViewValue(latLng);
       });
